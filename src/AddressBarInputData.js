599bbe73aa86559a996956869752da1cf0c0121a
shubh m jain <shubh.exe@gmail.com>
2019-01-06 18:24:59 +0530

    functionality to dynamically added params rows

--- src/AddressBarInputData.js
+++ src/AddressBarInputData.js
@@ -1,32 +1,57 @@
 import React, { Component } from 'react';
 
 class AddressBarInputData extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             enableParams: false,
             addRow: false,
+            paramRows: [(<tr>
+                <th scope="row"><input type='checkbox'></input></th>
+                <td><input type='text' onChange={(e) => e.target.value.length < 2 ? this.addParamsRow() : ''}
+                placeholder='New key'></input></td>
+                <td><input type='text' placeholder='Value'></input></td>
+                <td><input type='text' placeholder='Description'></input></td>
+            </tr>)],
             inputs: {
                 reqType: 'GET',
                 webApiUrl: '',
-                params: [{key: '', value: ''}]
+                params: [{ key: '', value: '' }]
             }
         };
         this.enableDisableParams = this.enableDisableParams.bind(this);
+        this.addParamsRow = this.addParamsRow.bind(this);
+        this.getParamsRows = this.getParamsRows.bind(this);
     }
 
     enableDisableParams() {
         this.setState(state => ({
             enableParams: !state.enableParams
         }));
+        console.log(this.state.paramRows)
+        // this.addParamsRow();
+    }
+
+    addParamsRow() {
+        const element = (<tr>
+            <th scope="row"><input type='checkbox'></input></th>
+            <td><input type='text' placeholder='New key'></input></td>
+            <td><input type='text' placeholder='Value'></input></td>
+            <td><input type='text' placeholder='Description'></input></td>
+        </tr>);
+        this.state.paramRows.push(element);
+    }
+
+    getParamsRows() {
+        return this.state.paramRows.map(row => row)
     }
 
     render() {
         return (
             <div>
                 <div>
-                    <select value={this.state.inputs.reqType} onChange={(e) => this.setState({inputs: {reqType: e.target.value}})} >
-                        <option value='GET' selected>GET</option>
+                    <select value={this.state.inputs.reqType} onChange={(e) => this.setState({ inputs: { reqType: e.target.value } })} >
+                        <option value='GET'>GET</option>
                         <option value='POST'>POST</option>
                         <option value='PUT'>PUT</option>
                         <option value='DELETE'>DELETE</option>
@@ -39,7 +64,7 @@ class AddressBarInputData extends Component {
                         <option>V9.0</option>
                         <option>V8.2</option>
                     </select> */}
-                    <input type='text' onChange={(e) => this.setState({inputs: {webApiUrl: e.target.value ? 'SomeURL' + e.target.value : ''}})}></input>
+                    <input type='text' onChange={(e) => this.setState({ inputs: { webApiUrl: e.target.value ? 'SomeURL' + e.target.value : '' } })}></input>
                     <button onClick={this.enableDisableParams}>Params</button>
                     <button>Send</button>
                 </div>
@@ -56,19 +81,23 @@ class AddressBarInputData extends Component {
                                     </tr>
                                 </thead>
                                 <tbody>
+                                    {/* TODO Bind key value and add to URL */}
+                                    {
+                                        this.state.paramRows.map(row => row)
+                                    }
+                                    {/* <tr>
+                                        <th scope="row"><input type='checkbox'></input></th>
+                                        <td><input type='text' placeholder='New key'></input></td>
+                                        <td><input type='text' placeholder='Value'></input></td>
+                                        <td><input type='text' placeholder='Description'></input></td>
+                                    </tr>
                                     <tr>
                                         <th scope="row"><input type='checkbox'></input></th>
-                                        <td><input type='text'></input></td>
-                                        <td><input type='text'></input></td>
-                                        <td><input type='text'></input></td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td><input type='text'></input></td>
-                                        <td><input type='text'></input></td>
-                                        <td><input type='text'></input></td>
-                                    </tr>
-                                    
+                                        <td><input type='text' placeholder='New key'></input></td>
+                                        <td><input type='text' placeholder='Value'></input></td>
+                                        <td><input type='text' placeholder='Description'></input></td>
+                                    </tr> */}
+
                                 </tbody>
                             </table>
                         </div>
